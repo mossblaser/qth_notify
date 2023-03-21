0f70170efaca23a0caaa3f537c25a06915c7102d
Jonathan Heathcote <mail@jhnet.co.uk>
2023-03-21 09:44:45 +0000

    Force timeout under Gnome shell and make None timeout consistent

--- qth_notify/__init__.py
+++ qth_notify/__init__.py
@@ -27,12 +27,24 @@ async def on_notify(_topic: str, message: Any) -> None:
             duration = message[2]
     
     notify = desktop_notify.aio.Notify(summary, body)
+    
     # In GNOME shell, prevent notifications accumulating in the notifications
     # drawer
     notify.set_hint("transient", Variant("i", 1))
-    if duration is not None:
+    
+    if duration is None:
+        notify.set_timeout(0)  # Never expire
+    else:
         notify.set_timeout(int(duration * 1000))
+    
     await notify.show()
+    
+    # In GNOME shell, notifications are not hidden after their timeout until
+    # they're touched by the mouse. To ensure the timeout is honoured,
+    # explicitly close it here.
+    if duration is not None:
+        await asyncio.sleep(duration)
+        await notify.close()
 
 
 async def async_main(path: str) -> None:
